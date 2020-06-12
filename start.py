f8694f79a8a5623830bcb0e0e5617ec5b1db1346
Jakob-Niklas See <git@nwex.de>
2020-06-12 20:11:47 +0200

    Added first server version

--- start.py
+++ start.py
@@ -1,129 +1,116 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 
-import getpass
-import sys
-import re
-from optparse import OptionParser
+import asyncio
+import json
+
+from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
 
 from minecraft import authentication
-from minecraft.exceptions import YggdrasilError
+from minecraft.authentication import AuthenticationToken
 from minecraft.networking.connection import Connection
-from minecraft.networking.packets import Packet, clientbound, serverbound
-from minecraft.compat import input
+from minecraft.networking.packets import Packet, serverbound, ChatMessagePacket, PlayerListItemPacket
+from minecraft.networking.packets.clientbound.play import PlayerListHeaderAndFooterPacket
+
+connections = dict()
 
 
-def get_options():
-    parser = OptionParser()
+class WebSocketServer(WebSocketServerProtocol):
+    username: str = ""
 
-    parser.add_option("-u", "--username", dest="username", default=None,
-                      help="username to log in with")
+    def onConnect(self, request):
+        print("Client connecting: {0}".format(request.peer))
 
-    parser.add_option("-p", "--password", dest="password", default=None,
-                      help="password to log in with")
+    def onOpen(self):
+        print("WebSocket connection open.")
 
-    parser.add_option("-s", "--server", dest="server", default=None,
-                      help="server host or host:port "
-                           "(enclose IPv6 addresses in square brackets)")
+    def sendPacket(self, packet: Packet):
+        if type(packet) is ChatMessagePacket:
+            self.sendMessage(packet.json_data.encode('utf8'), False)
 
-    parser.add_option("-o", "--offline", dest="offline", action="store_true",
-                      help="connect to a server in offline mode "
-                           "(no password required)")
+        if type(Packet) is PlayerListHeaderAndFooterPacket:
+            data = {"header": packet.header, "footer": packet.footer}
 
-    parser.add_option("-d", "--dump-packets", dest="dump_packets",
-                      action="store_true",
-                      help="print sent and received packets to standard error")
+            self.sendMessage(json.dumps(data).encode("utf8"), True)
 
-    (options, args) = parser.parse_args()
+        if type(Packet) is PlayerListItemPacket:
+            self.sendMessage(("%s" % packet).encode("utf8"), True)
 
-    if not options.username:
-        options.username = input("Enter your username: ")
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            message = json.loads(payload.decode('utf8'))
 
-    if not options.password and not options.offline:
-        options.password = getpass.getpass("Enter your password (leave "
-                                           "blank for offline mode): ")
-        options.offline = options.offline or (options.password == "")
+            if message["type"] == "login":
+                try:
+                    if message["username"] in connections and message["password"] == connections[message["username"]]["password"]:
+                        connections[message["username"]]["connection"].register_packet_listener(self.sendPacket, Packet, early=True)
 
-    if not options.server:
-        options.server = input("Enter server host or host:port "
-                               "(enclose IPv6 addresses in square brackets): ")
-    # Try to split out port and address
-    match = re.match(r"((?P<host>[^\[\]:]+)|\[(?P<addr>[^\[\]]+)\])"
-                     r"(:(?P<port>\d+))?$", options.server)
-    if match is None:
-        raise ValueError("Invalid server address: '%s'." % options.server)
-    options.address = match.group("host") or match.group("addr")
-    options.port = int(match.group("port") or 25565)
+                        print("Resuming session as " + connections[message["username"]]["auth_token"].username + " on " + message["host"])
+                        return
 
-    return options
+                    print("Trying to log in as " + message["username"] + " on " + message["host"] + ":" + message["port"])
 
+                    auth_token = authentication.AuthenticationToken()
+                    auth_token.authenticate(message["username"], message["password"])
 
-def main():
-    options = get_options()
+                    connection = Connection(message["host"], int(message["port"]), auth_token=auth_token)
+                    connection.register_packet_listener(self.sendPacket, Packet, early=True)
+                    connection.connect()
 
-    if options.offline:
-        print("Connecting in offline mode...")
-        connection = Connection(
-            options.address, options.port, username=options.username)
-    else:
-        auth_token = authentication.AuthenticationToken()
-        try:
-            auth_token.authenticate(options.username, options.password)
-        except YggdrasilError as e:
-            print(e)
-            sys.exit()
-        print("Logged in as %s..." % auth_token.username)
-        connection = Connection(
-            options.address, options.port, auth_token=auth_token)
+                    connections[message["username"]] = {
+                        "password": message["password"],
+                        "host": message["host"],
+                        "port": message["port"],
+                        "auth_token": auth_token,
+                        "connection": connection
+                    }
 
-    if options.dump_packets:
-        def print_incoming(packet):
-            if type(packet) is Packet:
-                # This is a direct instance of the base Packet type, meaning
-                # that it is a packet of unknown type, so we do not print it.
-                return
-            print('--> %s' % packet, file=sys.stderr)
+                    self.username = message["username"]
 
-        def print_outgoing(packet):
-            print('<-- %s' % packet, file=sys.stderr)
+                    print("Started new session as " + auth_token.username + " on " + message["host"])
 
-        connection.register_packet_listener(
-            print_incoming, Packet, early=True)
-        connection.register_packet_listener(
-            print_outgoing, Packet, outgoing=True)
+                except Exception as e:
+                    print("Error while logging in: " + repr(e))
+                    self.sendMessage(("Error while logging in: " + repr(e)).encode('utf8'), False)
+                    self._closeConnection()
 
-    def handle_join_game(join_game_packet):
-        print('Connected.')
-
-    connection.register_packet_listener(
-        handle_join_game, clientbound.play.JoinGamePacket)
-
-    def print_chat(chat_packet):
-        print("Message (%s): %s" % (
-            chat_packet.field_string('position'), chat_packet.json_data))
-
-    connection.register_packet_listener(
-        print_chat, clientbound.play.ChatMessagePacket)
-
-    connection.connect()
-
-    while True:
-        try:
-            text = input()
-            if text == "/respawn":
-                print("respawning...")
+            elif message["type"] == "respawn":
                 packet = serverbound.play.ClientStatusPacket()
                 packet.action_id = serverbound.play.ClientStatusPacket.RESPAWN
-                connection.write_packet(packet)
-            else:
+                connections[self.username]["connection"].write_packet(packet)
+
+            elif message["type"] == "chat":
                 packet = serverbound.play.ChatPacket()
-                packet.message = text
-                connection.write_packet(packet)
-        except KeyboardInterrupt:
-            print("Bye!")
-            sys.exit()
+                packet.message = message["message"]
+                connections[self.username]["connection"].write_packet(packet)
+
+            elif message["type"] == "disconnect":
+                connections[self.username]["connection"].disconnect()
+                connections.pop(self.username)
+                self._closeConnection()
+
+    def onClose(self, wasClean, code, reason):
+        connections[self.username]["connection"].packet_listeners.clear()
+        connections[self.username]["connection"].outgoing_packet_listeners.clear()
+        connections[self.username]["connection"].early_packet_listeners.clear()
+        connections[self.username]["connection"].early_outgoing_packet_listeners.clear()
+
+        print("Cleared registered listeners")
+
+        print("WebSocket connection closed: {0}".format(reason))
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    factory = WebSocketServerFactory("ws://127.0.0.1:9000")
+    factory.protocol = WebSocketServer
+
+    loop = asyncio.get_event_loop()
+    coro = loop.create_server(factory, '0.0.0.0', 9000)
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        loop.close()
