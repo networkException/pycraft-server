ce7fa55ac946d35c3455a4033c93d98a374862f4
networkException <git@nwex.de>
2020-09-03 19:49:18 +0200

    Meta: Added update script for restarting the server and all clients

---
+++ update.sh
@@ -0,0 +1,19 @@
+# stop server
+screen -X -S pycraft-server quit
+
+# stop clients
+pm2 stop pycraft-client-2
+pm2 stop pycraft-client-4
+
+#update clients
+npm run update --prefix /home/networkexception/projects/minecraft/pycraft-client-2
+npm run update --prefix /home/networkexception/projects/minecraft/pycraft-client-4
+
+# start server
+/home/networkexception/projects/minecraft/pycraft-server/run.sh
+
+# start clients
+pm2 start pycraft-client-2
+pm2 start pycraft-client-4
+
+
