a68649cbe8e1a938405198b35aaaeb42e1b9224c
Jakob-Niklas See <git@nwex.de>
2020-10-01 16:30:53 +0200

    Update: Removed pm2 status output

--- update.sh
+++ update.sh
@@ -1,12 +1,14 @@
 # stop server
 screen -X -S pycraft-server quit
+echo "stopped server"
 
 # update server
 git pull
 
 # stop clients
-pm2 stop pycraft-client-2
-pm2 stop pycraft-client-4
+pm2 stop pycraft-client-2 -s
+pm2 stop pycraft-client-4 -s
+echo "stopped clients"
 
 # update clients
 npm run update --prefix /home/networkexception/projects/minecraft/pycraft-client-2
@@ -16,7 +18,7 @@ npm run update --prefix /home/networkexception/projects/minecraft/pycraft-client
 /home/networkexception/projects/minecraft/pycraft-server/run.sh
 
 # start clients
-pm2 start pycraft-client-2
-pm2 start pycraft-client-4
-
+pm2 start pycraft-client-2 -s
+pm2 start pycraft-client-4 -s
+echo "started clients"
 
